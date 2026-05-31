ced1e848ea6ecc31425c493b76aa2fe3dc79e68b
rikiandreas <rikiandreas073@gmail.com>
2026-05-31 15:15:04 +0700

    Add files via upload

---
+++ MLProject/modelling.py
@@ -0,0 +1,54 @@
+
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score
+)
+
+mlflow.set_tracking_uri("file:///content/mlruns")
+mlflow.set_experiment("Breast Cancer Classification")
+
+df = pd.read_csv("breast_cancer_preprocessing.csv")
+
+if "id" in df.columns:
+    df = df.drop("id", axis=1)
+
+X = df.drop("diagnosis", axis=1)
+y = df["diagnosis"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
+
+mlflow.sklearn.autolog()
+
+with mlflow.start_run():
+
+    model = LogisticRegression(max_iter=1000)
+
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+
+    print("Accuracy :", accuracy)
+    print("Precision:", precision)
+    print("Recall   :", recall)
+    print("F1 Score :", f1)
+
+print("Training selesai dan model tercatat di MLflow")
